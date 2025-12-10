9d64bfa350e745b2008c1bf7fac3f212ef819063
stherm <steffen_hermann97@yahoo.de>
2025-12-10 16:49:19 +0100

    updated disk.sh

--- hosts/h-dev-main/disks.sh
+++ hosts/h-dev-main/disks.sh
@@ -1,50 +1,35 @@
 #!/usr/bin/env bash
-
+set -e
 SSD='/dev/sda'
 MNT='/mnt'
 SWAP_GB=4
 
-# Helper function to wait for devices
 wait_for_device() {
-  local device=$1
-  echo "Waiting for device: $device ..."
-  while [[ ! -e $device ]]; do
-    sleep 1
-  done
-  echo "Device $device is ready."
+  while [[ ! -e $1 ]]; do sleep 0.1; done
 }
 
-swapoff --all
+swapoff --all || true
 udevadm settle
 wait_for_device $SSD
 
-echo "Wiping filesystem on $SSD..."
 wipefs -a $SSD
 
-echo "Creating new MBR partition table on $SSD..."
 fdisk $SSD << EOF
-o
-w
-EOF
-
-echo "Partitioning $SSD..."
-fdisk $SSD << EOF
+g
 n
-p
 1
 
 +512M
-a
+t
+1
 n
-p
 2
 
 +${SWAP_GB}G
 t
 2
-82
+19
 n
-p
 3
 
 
@@ -57,18 +42,13 @@ wait_for_device "${SSD}1"
 wait_for_device "${SSD}2"
 wait_for_device "${SSD}3"
 
-echo "Formatting partitions..."
-mkfs.ext4 -L BOOT "${SSD}1"
+mkfs.vfat -F 32 -n BOOT "${SSD}1"
 mkswap -L SWAP "${SSD}2"
 mkfs.ext4 -L ROOT "${SSD}3"
 
-echo "Mounting partitions..."
 mount -o X-mount.mkdir "${SSD}3" "$MNT"
 mkdir -p "$MNT/boot"
 mount "${SSD}1" "$MNT/boot"
-
-echo "Enabling swap..."
 swapon "${SSD}2"
 
-echo "Partitioning and setup complete:"
 lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL
